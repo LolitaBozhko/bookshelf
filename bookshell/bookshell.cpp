adc8f40f5f9e155d539cf98a4beca4c4041c3b79
LolitaBozhko <63454136+LolitaBozhko@users.noreply.github.com>
2020-09-15 17:56:49 +0300

    version1

---
+++ bookshell/bookshell.cpp
@@ -0,0 +1,35 @@
+﻿// bookshell.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+
+#ifndef book_h
+#define book_h
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Book {
+
+public:
+	string Name;
+	int Pages;
+	Book() {
+		Name = '-';
+		Pages = 0;
+	}
+	
+	string gerName() {
+		return Name;
+	}
+	
+	int getPages() {
+		return Pages;
+	}
+	void Print() {
+		cout << "Book: " << Name << ", p." << Pages << "\n";
+	}
+};
+#endif
+
+
